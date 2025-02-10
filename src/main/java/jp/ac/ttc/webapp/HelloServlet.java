b373ee1d4326dd3457910c323be4001bc6765723
ts23304009 <ts23304009@ga.ttc.ac.jp>
2025-02-10 21:25:07 +0900

    変更

--- src/main/java/jp/ac/ttc/webapp/HelloServlet.java
+++ src/main/java/jp/ac/ttc/webapp/HelloServlet.java
@@ -14,6 +14,6 @@ public class HelloServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setContentType("text/html");
 		PrintWriter out = resp.getWriter();
-		out.write("<h1>Hello, world!!!!!!</h1>");
+		out.write("<h1>Hello, world!</h1>");
 	}
 }
